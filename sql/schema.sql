3c6a52b4d60a2051d8fa4e2280e34d66d7ae393a
Roman Ponomarev <xgerfy@gmail.com>
2026-03-19 20:44:16 +0400

    add up-migration for db, refactor schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,25 +1,23 @@
-CREATE
-DATABASE IF NOT EXIST basic_web CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE
-basic_web;
+CREATE DATABASE IF NOT EXISTS basic_web CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE basic_web;
 
-CREATE TABLE users
+CREATE TABLE IF NOT EXISTS users
 (
     id         INT AUTO_INCREMENT PRIMARY KEY,
-    email      varchar(255) NOT NULL UNIQUE,
+    email      VARCHAR(255) NOT NULL UNIQUE,
     password   VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE feedback
+CREATE TABLE IF NOT EXISTS feedback
 (
     id         INT AUTO_INCREMENT PRIMARY KEY,
     user_id    INT          NOT NULL,
     subject    VARCHAR(255) NOT NULL,
     message    TEXT         NOT NULL,
-    priority   ENUM('low', 'medium', 'high') DEFAULT 'medium',
+    priority   ENUM ('low', 'medium', 'high') DEFAULT 'medium',
     topics     TEXT,
     category   VARCHAR(50),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP                      DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
